31cf64b4fd3cb8c01d118b94ec81708cb552c471
Trevor Mearns <spiral272@gmail.com>
2020-04-24 15:55:53 +0900

    data now rendering correctly post refactoring but delete functionality is not work. Keeps returnins 'i is undefined' :-(

--- brainsOfTheOperation.js
+++ brainsOfTheOperation.js
@@ -5,35 +5,41 @@ let firstExpense = null;
 let secondExpense = null;
 let expenseAmount = null;
 const tableValues = [];
-
-let description = document.getElementById("what").value;
-let place = document.getElementById("where").value;
-let date = document.getElementById("when").value;
-let amount = document.getElementById("howMuch").value;
-let newItem = {
-    id: tableValues.length > 0 ? tableValues[tableValues.length -1].id + 1 : 1,
-    item: description,
-    time: date,
-    place: place,
-    total: parseFloat(amount)
-};
+let description = document.getElementById("what");
+let place = document.getElementById("where");
+let date = document.getElementById("when");
+let amount = document.getElementById("howMuch");
 
 
-expenseAmount = newItem.total;
 
-document.getElementById("submitButton").addEventListener("click", createNewItem);
+
+
+document
+    .getElementById("submitButton")
+    .addEventListener("click", createNewItem);
 
 
 function createNewItem(e) {
     e.preventDefault();
-    populateTable();
-   
 
+    let newItem = {
+        id: tableValues.length > 0 ? tableValues[tableValues.length -1].id + 1 : 1,
+        item: description.value,
+        time: date.value,
+        place: place.value,
+        total: amount.value
+    };
+    expenseAmount = parseFloat(newItem.total);
+    populateTable(newItem);
+   
     document.getElementById("what").value = "";
     document.getElementById("where").value = "";
     document.getElementById("when").value = "";
     document.getElementById("howMuch").value = "";
-    activateDelete();
+ 
+    tableValues.push(newItem);
+    console.log("expenseAmount is ", expenseAmount);
+    console.log("tableValues ", tableValues);
 }
 function getFirstExpense(expenseAmount) {
     firstExpense === null ? firstExpense = expenseAmount : firstExpense += expenseAmount;
@@ -61,17 +67,30 @@ function subtractExpense() {
 
 
 function activateDelete() {
-    document.getElementById('delete-button').addEventListener('click', deleteRow);
+    document.getElementById('delete-button').addEventListener('click', function() { deleteRow(tableValues[0].id); });
 }
-function deleteRow() {
-    console.log("deleteRow fired");
-    let row = document.getElementById('table-row');
-    row.parentNode.removeChild(row);
+
+function deleteRow(id) {
+        console.log("deleteRow fired");
+        console.log("tableValues before loop is", tableValues)
+        id = tableValues[i].id;
+        for(let i = 0; i < tableValues.length; i ++) {
+            if(tableValues[i].id == id) {
+                tableValues.splice(i, 1);
+            }
+        }
+        let row = document.getElementById('table-row');
+        row.parentNode.removeChild(row);
+        console.log("tableValues after loop is", tableValues)
+     
+    }; 
+
+
     
 
-}
 
-function populateTable() {
+
+function populateTable(newItem) {
   
     //expenseAmount = newItem.total;//
     // creates new row to be injected into html table
@@ -93,14 +112,18 @@ function populateTable() {
     deleteButton.setAttribute('id', 'delete-button');
     tableRow.setAttribute('id', 'table-row');
     
-    
+    let item = newItem.item;
     
     // calls values of newItem object as text values for new table row
-    tableCell1.textContent = `${newItem.item}`;
+    tableCell1.textContent = `${item}`;
+    tableCell1.setAttribute('id', `${newItem.id}`);
     tableCell2.textContent = `${newItem.place}`;
     tableCell3.textContent = `${newItem.time}`;
     tableCell4.textContent = `${newItem.total}`;
     console.log("New row id is ",newItem.id);
+    console.log("New row item is ",newItem.item);
+    console.log("New row place is ",newItem.place);
+    console.log("New row total is ",newItem.total);
 
     
  
@@ -119,4 +142,8 @@ function populateTable() {
     firstExpense = expenseAmount;
     secondExpense = null;
     console.log("expense amount is ", expenseAmount);
+    activateDelete();
+
 }
+
+
